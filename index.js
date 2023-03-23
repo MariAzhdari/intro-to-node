7f4a4c982e8507a376280895b4b2617c52289152
Mari Azh <Marzi.aghchay@gmail.com>
2023-03-23 12:05:00 +0000

    .

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 //jshint eversion:6
-
-const fs = require("fs");
-
-fs.copyFileSync("file1.txt","file2.txt");
+const superheroes = require("superheroes");
+ var mySuperHero = superheroes.random();
+console.log(mySuperHero);
 
